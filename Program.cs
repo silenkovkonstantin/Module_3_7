f5e0e040a476f381e5a8bb4b034b7695338a098c
silenkovkonstantin <silenkov@inbox.ru>
2023-01-11 19:32:49 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите имя: ");
+        var name = Console.ReadLine();
+        Console.Write("Введите возраст: ");
+        var age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Твое имя {0} и возраст {1}", name, age);
+        Console.Write("Введите дату рождения: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Твоя дата рождения {0}", birthdate);
+        Console.ReadKey();
+    }
+}
